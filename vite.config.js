739bb534152d7a1e7f7c3e22d52dbbe43491f55a
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-05-13 23:10:09 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/bc_react_68_nadia-yanushevska/",
     build: {
         sourcemap: true,
     },
